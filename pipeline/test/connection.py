ba441606d5c0e0773090b147bed75f72c7f80932
nobrain711 <qa9975@ync.ac.kr>
2026-03-06 18:25:23 +0900

    style(lint): fix pylint warnings (C0114, C0304)
    - module docstring 추가
    - 파일 마지막 newline 추가
    
    Refs: #5 #7

--- pipeline/test/connection.py
+++ pipeline/test/connection.py
@@ -1,3 +1,24 @@
+"""
+=========================================================================
+Project:
+- <project-name>
+
+Module:
+- pipeline/test
+
+File: connection.py
+
+Purpose:
+- MySQL 데이터베이스 연결 테스트
+- Docker 환경에서 MySQL 컨테이너 연결 상태 확인
+
+Author: (조동휘)
+Created: 2026-03-06
+
+Updated:
+- 2026-03-06: initial version (조동휘)
+=========================================================================
+"""
 import pymysql
 
 conn = pymysql.connect(
